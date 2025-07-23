8374510ed81fc4c02e9962ff2e7a046f8010f6d9
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-07-23 23:21:48 +0700

    test sync pause 7 minutes

--- src/services/bus-scheduler/bus-scheduler.service.ts
+++ src/services/bus-scheduler/bus-scheduler.service.ts
@@ -275,7 +275,7 @@ export class BusSchedulerService implements OnModuleInit {
     }
   }
 
-  @Cron('30 23 * * *', {
+  @Cron('40 23 * * *', {
     name: 'daily_product_sync',
     timeZone: 'Asia/Ho_Chi_Minh',
   })
