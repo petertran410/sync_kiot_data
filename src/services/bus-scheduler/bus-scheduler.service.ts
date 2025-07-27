2ba07beb7d0c34976602c458574948587ddd6698
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-07-27 12:39:38 +0700

    Change 23h time => 21

--- src/services/bus-scheduler/bus-scheduler.service.ts
+++ src/services/bus-scheduler/bus-scheduler.service.ts
@@ -275,7 +275,7 @@ export class BusSchedulerService implements OnModuleInit {
     }
   }
 
-  @Cron('15 14 * * *', {
+  @Cron('0 21 * * *', {
     name: 'daily_product_sync',
     timeZone: 'Asia/Ho_Chi_Minh',
   })
