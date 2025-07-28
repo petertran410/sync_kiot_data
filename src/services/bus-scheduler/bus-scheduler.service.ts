d8f2440a7830d01af0b4588e729ee677f251889c
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-07-28 18:01:40 +0700

    Add lineNumber to fix missing data

--- src/services/bus-scheduler/bus-scheduler.service.ts
+++ src/services/bus-scheduler/bus-scheduler.service.ts
@@ -275,7 +275,7 @@ export class BusSchedulerService implements OnModuleInit {
     }
   }
 
-  @Cron('0 23 * *', {
+  @Cron('0 23 * * *', {
     name: 'daily_product_sync',
     timeZone: 'Asia/Ho_Chi_Minh',
   })
