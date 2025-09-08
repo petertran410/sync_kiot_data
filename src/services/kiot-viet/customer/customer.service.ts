34a881c1304e29d8c97c4eb6e15d463ab1d781a3
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-09-08 18:36:08 +0700

    reduce time recent sync for order and customer

--- src/services/kiot-viet/customer/customer.service.ts
+++ src/services/kiot-viet/customer/customer.service.ts
@@ -493,7 +493,7 @@ export class KiotVietCustomerService {
         }
 
         const lastModifiedDate = new Date();
-        lastModifiedDate.setDate(lastModifiedDate.getDate() - 6);
+        lastModifiedDate.setDate(lastModifiedDate.getDate() - 4);
         const lastDate = lastModifiedDate.toISOString().split('T')[0];
 
         const dateEnd = new Date();
