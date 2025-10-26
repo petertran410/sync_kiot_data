60b42d51b41b435cb11500689ab5559aff75aa79
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-10-27 00:09:46 +0700

    Update data upsert

--- src/services/webhook/webhook.module.ts
+++ src/services/webhook/webhook.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { WebhookService } from './webhook.service';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { HttpModule } from '@nestjs/axios';
+import { KiotVietModule } from '../kiot-viet/kiot-viet.module';
 
 @Module({
-  imports: [PrismaModule, HttpModule],
+  imports: [PrismaModule, HttpModule, KiotVietModule],
   providers: [WebhookService],
   exports: [WebhookService],
 })
