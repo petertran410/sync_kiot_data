466f1f1c42098d886a817fbf3adf91542925c4c4
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2025-11-01 15:14:14 +0700

    Add invoiceKiotVietId && productKiotVietId

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NumberFormatInterceptor } from './interceptors/number-format.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(new NumberFormatInterceptor());
   await app.listen(process.env.PORT ?? 8083);
 }
 bootstrap();
